d491112ae30c084346e9f709aa501d65e131b66f
David Callizaya <davidcallizaya@gmail.com>
2020-11-07 00:32:18 -0400

    Fix code style

--- src/routes/NextStep.vue
+++ src/routes/NextStep.vue
@@ -3,16 +3,18 @@
     <table class="table table-bordered">
       <thead>
         <tr>
-          <th scope="col"></th>
+          <th scope="col"/>
           <th scope="col">Status</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(task, i) in tasks" :key="i">
           <td>
-            <router-link :to="{path: task.path, query: task.token}">{{task.name}}</router-link>
+            <router-link :to="{ path: task.path, query: task.token }">{{
+              task.name
+            }}</router-link>
           </td>
-          <td>{{task.status}}</td>
+          <td>{{ task.status }}</td>
         </tr>
       </tbody>
     </table>
@@ -20,49 +22,49 @@
 </template>
 
 <script>
-import workflowMixin from "../components/mixins/workflow";
+import workflowMixin from '../components/mixins/workflow';
 
 export default {
-  name: "NextStep",
-  path: "/process/next",
+  name: 'NextStep',
+  path: '/process/next',
   mixins: [workflowMixin],
   data() {
     return {
-      tasks: []
+      tasks: [],
     };
   },
   methods: {
     updateTasks(tasks) {
       const actives = [];
-      tasks.forEach(task => {
-        task.status === "ACTIVE" ? actives.push(task) : null;
+      tasks.forEach((task) => {
+        task.status === 'ACTIVE' ? actives.push(task) : null;
       });
       if (actives.length === 1 && actives[0].path) {
         this.openTask(tasks[0]);
       } else if (tasks.length === 0) {
         this.gotoDashboard();
       } else {
-        this.$set(this, "tasks", tasks);
+        this.$set(this, 'tasks', tasks);
       }
     },
     loadTasks() {
-      this.processTasks(this.workflowToken).then(response => {
+      this.processTasks(this.workflowToken).then((response) => {
         const tasks = response.data.response;
         this.updateTasks(tasks);
       });
-    }
+    },
   },
   beforeRouteEnter(to, from, next) {
-    next(vm => {
+    next((vm) => {
       vm.loadTasks();
     });
   },
   mounted() {
     const instance = this.workflowToken.instance;
     const channel = `Process.${instance}`;
-    this.addSocketListener(channel, ".ProcessUpdated", event => {
+    this.addSocketListener(channel, '.ProcessUpdated', (event) => {
       this.updateTasks(event.tasks);
     });
-  }
+  },
 };
 </script>
